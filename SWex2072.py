9cb450b7e5a514851b2eadfc0e5015085e343a92
silverjjj <silver_jae@naver.com>
2020-01-25 18:36:17 +0900

    20200125

---
+++ SWex2072.py
@@ -0,0 +1,22 @@
+T = list(input())
+
+result = 0
+for i in range(0,len(T)):
+    if int(T[i]) % 2 == 1
+        result += int(T[i])
+    n1, n2, n3, n4, n5, n6, n7, n8, n9, n10 = map(int, input().split())
+    if (n1, n2, n3, n4, n5, n6, n7, n8, n9, n10) % 2 ==1:
+        sum(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+        print(sum)
+
+
+
+# T = int(input())
+# for i in range(0,T):
+#     n1, n2, n3, n4, n5, n6, n7, n8, n9, n10 = map(int, input().split())
+#     if (n1, n2, n3, n4, n5, n6, n7, n8, n9, n10) % 2 ==1:
+#         sum(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+#         print(sum)
+#
+
+# print(f"#{i+1}",max(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10))
