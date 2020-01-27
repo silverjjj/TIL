76a82326778366b9d10d66c79c22a87d7879125b
silverjjj <silver_jae@naver.com>
2020-01-27 17:52:34 +0900

    20200127

--- SWex1966.py
+++ SWex1966.py
@@ -1,14 +1,26 @@
-T = int(input())
-N = int(input())
-
-for test_case in range(1, T+1):
-    num = list(map(int, input().split()))
-    print(f"#{test_case} ", end=" ")
-    if len(num) == N:
-        num.sort()
-        for i in range(0,len(num)):
-            print(num[i], end=" ")
-    else:
-        0
-    print()
-
+# N = int(input())
+# x = N / 10          # 10의 자리
+# y = N % 10          # 1의 자리
+# for i in range(1,N+1):
+#     x = i / 10      # 입력값의 10의 자리
+#     y = i % 10      # 입력값의 1의 자리
+#     if x % 3 == 0 or x % 6 == 0 or x % 9 == 0:
+#         print("-",end=" ")   # 10의자리가 369의 배수일경우 -로 출력
+#     elif x % 3 != 0 or x % 6 != 0 or x % 9 != 0:
+#         print(i, end = " ")
+#     elif y % 3 == 0 or y % 6 == 0 or y % 9 == 0:
+#         print("-",end=" ")
+#     elif y % 3 != 0 or y % 6 != 0 or y % 9 != 0:
+#         print(i, end=" ")
+# a=int(input())
+# my_str="1"
+# for i in range(2, a+1):
+#     cnt=0
+#     for j in range(0,len(str(i))):
+#         if str(i)[j]=='3' or str(i)[j]=='6' or str(i)[j]=='9':
+#             cnt+=1
+#     if cnt==0:
+#         my_str+=" "+str(i)
+#     else:
+#         my_str+=" "+'-'*cnt
+# print(my_str)
--- SWex1984.py
+++ SWex1984.py
@@ -1,13 +1,13 @@
 T = int(input())
-count = 0
-for test_case in range(1,T+1):
-    while T > count:
+count=0
+while T >= count:
+    for test_case in range(1,T+1):
         count +=1
         num = list(map(int,input().split()))
-        if len(num) == 10:
-            num.sort()
-            result =0
-            for i in range(1,9):
-                result += num[i]
-        print(f"#{count}",round(result/8))
+        num.sort()
+        result =0
+        for i in range(1,9):
+            result += num[i]
+        print(f"#{test_case}",round(result/8))
+    break
 
