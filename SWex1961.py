4bd87da675d0a3a1d1bfa33f8ec694c46ecec977
silverjjj <silver_jae@naver.com>
2020-02-08 22:25:15 +0900

    20200208

--- SWex1961.py
+++ SWex1961.py
@@ -1,32 +1,41 @@
 
-
-# for x in zip(*my_list):
-#     my_list_transpose.append(x)
-# my_list_transpose = []
-# my_list_transpose = [x for x in zip(*my_list)]
-# my_reverse = []
-# for i in range(len(my_list_transpose)):
-#     my_reverse.append(list(reversed(my_list_transpose[i])))
-# print(my_reverse)
-
-def arrspin(my_list):
-    my_reverse = []
-    result = []
-    index = 0
-    my_list_transpose = [x for x in zip(*my_list)]
-    for i in range(len(my_list_transpose)):
-        my_reverse.append(list(reversed(my_list_transpose[i])))
-        result[index].append(''.join(list(reversed(my_list_transpose[i]))))
-        index +=1
-
-    return print(result)
-
 N = int(input())
-my_list = []
-for j in range(N):
-    my = list(map(str, input().split()))
-    my_list.append(my)
+nums = [0 for _ in range(N)]
+for i in range(N):
+    nums[i] = list(map(int,input().split()))
+result = [0 for _ in range(N)]
+for i in range(N):
+    print(nums[0:][-1-i+N])
+    #nums[i][0:] = result[0:][i+N-1]
+print(nums)
+print(result)
 
-arrspin(my_list)
 
+
+
+
+
+
+
+
+# def arrspin(my_list):
+#     my_reverse = []
+#     result = []
+#     index = 0
+#     my_list_transpose = [x for x in zip(*my_list)]
+#     for i in range(len(my_list_transpose)):
+#         my_reverse.append(list(reversed(my_list_transpose[i])))
+#         result[index].append(''.join(list(reversed(my_list_transpose[i]))))
+#         index +=1
+#
+#     return print(result)
+#
+# N = int(input())
+# my_list = []
+# for j in range(N):
+#     my = list(map(str, input().split()))
+#     my_list.append(my)
+#
 # arrspin(my_list)
+#
+# # arrspin(my_list)
---
+++ SWex6190.py
@@ -0,0 +1,25 @@
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    maxV = -1           # 최대값을 찾기위한 변수
+
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            num = arr[i] * arr[j]
+            if i < j:           #문제에서 주어진 조건
+                num_copy = num  #원본숫자를 조작 -> 백업
+                pre = 10
+                islnc = True
+                while num:
+                    n = num % 10    #1의자리를 얻을수있다.
+                    if pre < n:     #n이 이전 수보다 크면 -> 단조증가가아님
+                        islnc = False
+                        break
+                    num = num // 10
+                    pre = n
+                if islnc:
+                     if maxV < num_copy:
+                         maxV = num_copy
+
+    print("#{} {}".format(tc, maxV))
