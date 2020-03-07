2d227899d5dd85fd77bde1c727bd832162b4a705
silverjjj <silver_jae@naver.com>
2020-03-07 17:46:51 +0900

    20200307

--- [SSAFY_Live] Python A 대비반/3월 6일/가능한 시험점수.py
+++ [SSAFY_Live] Python A 대비반/3월 6일/가능한 시험점수.py
@@ -1,17 +1,16 @@
-#SWex3752 가능한 시험점수
+# SWex3752 가능한 시험점수
 
-T= int(input())
-for tc in range(1,T+1):
+T = int(input())
+for tc in range(1, T + 1):
     N = int(input())
-    num = list(map(int,input().split()))
+    num = list(map(int, input().split()))
     s = {0}
-    
     for i in num:
         for j in list(s):
             # print(s)
-            s.add(j+i)
+            s.add(j + i)
 
-    print("#{} {}".format(tc,len(s)))
+    print("#{} {}".format(tc, len(s)))
 
 
 
