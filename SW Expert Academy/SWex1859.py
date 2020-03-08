a1cc24401690ad42df9bddd741c368c8e12a858b
silverjjj <silver_jae@naver.com>
2020-03-08 19:29:15 +0900

    20200308

--- SW Expert Academy/SWex1859.py
+++ SW Expert Academy/SWex1859.py
@@ -1,19 +1,11 @@
-N = int(input())
-case = list(map(int,input().split()))
-cases = []
-a = cnt =0
-next = 1
-max(case)
-for i in range(N):
-    print(case[i])
-    if case[i] >= next:
-        cases.append(case[i])
-        next = case[i]
-        cnt +=1
-    elif case[i] < next:
-        a += next*cnt - sum(cases)
-        cases.append(case[i])
-        print(a, cases)
-        next = 1
-        cnt = 0
-
+for tc in range(1,1+int(input())):
+    N = int(input())
+    num = map(int,input().split())
+    last = num[-1]
+    sum = 0
+    for i in range(N-1,-1,-1):
+        if last > num[i]:
+            sum += (last - num[i])
+        else:
+            last = num[i]
+    print("#{} {}".format(tc ,sum))
