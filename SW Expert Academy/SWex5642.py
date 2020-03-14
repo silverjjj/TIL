0068db63aed751b6573f277a65a79323487ee54d
1193 <silver_jae@naver.com>
2020-03-14 22:57:27 +0900

    20200314

--- SW Expert Academy/SWex5642.py
+++ SW Expert Academy/SWex5642.py
@@ -1,35 +1,19 @@
 # SWex5642. [Professional] 합
 T = int(input())
-sum_list = []
 for tc in range(T):
     n = int(input())
-    num = list(map(int,input().split()))
-    maxV = 1
-    # for i in range(n):
-    #     sum = 0
-    #     for j in range(i,n):
-    #         print(num[j],end=" ")
-    #     print()
-    # print()
-    for k in range(n):
-        for i in range(k,n):
+    num = list(map(int, input().split()))
+    maxV = -1234
+    if max(num) < 0:
+        maxV = max(num)
+    else:
+        for j in range(n):
             sum = 0
-            for j in range(k,i+1):
-                # print(num[j],end=" ")
-                sum += num[j]
-                print(num[j],end = " ")
-            print()
-            print("합 : ", sum)
-            if sum>maxV:
-                maxV =sum
-        print()
-    #         print()
-    #     print()
-    # print()
-    #         sum += num[j]
-    #         if sum > maxV:
-    #             maxV = sum
+            for k in range(j, n):
+                sum += num[k]
+                if sum < 0:
+                    break
+                if sum > maxV:
+                    maxV = sum
+
     print("#{} {}".format(tc + 1, maxV))
-#     sum_list.append(maxV)
-# for tc in range(T):
-#     print("#{} {}".format(tc+1, sum_list[tc]))
