e668cb5638fb3ec0adde239a2e4bb80971b803b4
1193 <silver_jae@naver.com>
2020-03-17 17:32:04 +0900

    20200317

--- SW Expert Academy/SWex1226.py
+++ SW Expert Academy/SWex1226.py
@@ -1,28 +1,27 @@
 # SWex1226 미로1
+# 내가 자꾸 스택안쓰고 visited 으로만 풀려다가 꼬였음.
 def f(x,y):
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
-
-    sx = x
-    sy = y
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < 16 and 0 <= ny < 16:
-            if miro[nx][ny] == 0 and visited[nx][ny] == 0:
-                visited[sx][sy] = 1
-                f(nx, ny)
-                visited[sx][sy] = 0
-                f(nx, ny)
-            elif miro[nx][ny] == 3:
-                return 1
+    s = []
+    s.append([x,y])
+    visited[x][y] =1
+    while len(s) > 0:
+        ad = s.pop()
+        for k in range(4):      # 4방향으로 ㄱㄱ
+            nx = ad[0] + dx[k]
+            ny = ad[1] + dy[k]
+            # print(nx,ny)
+            if miro[nx][ny] == 3:
+                return "1"
+            elif miro[nx][ny] == 0 and visited[nx][ny] == 0:
+                s.append([nx,ny])
+                visited[nx][ny] = 1
+    return "0"
 
 for tc in range(1,11):
     t = int(input())
-    miro = [list(map(int, input().split())) for _ in range(16)]
-    visited = [[0]*16 for _ in range(16)]
-    result = 0
-    # for row in miro:
-    #     print(row)
-    f(1,1)
-    print("#{} {}".format(tc,result))
+    n = 100
+    miro = [list(map(int, input())) for _ in range(n)]
+    visited = [[0]*n for _ in range(n)]
+    print("#{} {}".format(tc,f(1,1)))
--- SW Expert Academy/SWex6109.py
+++ SW Expert Academy/SWex6109.py
@@ -1,42 +1,64 @@
 # SWex6109. 추억의 2048게임
-def f(N,rm):
-    for i in range(N):
-        for j in range(N):
-            rm[i][j] = rm[j][i]
-def go(N,rm):
-    # s = []
-    # s1 = []
-    # for i in range(N):
-    #     for j in range(N):
-    #         s.append(rm[i][j])
-    #
-    #     if s[-1] == s[-2]:
-    #         s1.append(s[-1]*2)
-    #         s.pop()
-    #         s.pop()
-    #     else:
-    #         s1.append(s[-1])
-    #
+#
 
-n = input().split()
-N= int(n[0])
-S= n[1]
-rm = [list(map(int,input().split())) for _ in range(N)]
-for row in rm:
-    print(row)
-if S =='Right':
-    go(N,rm)
-elif S=='Up':
-    rm = f(N,rm)
-    go(N,rm)
+for tc in range(int(input())):
+    def f(N,rm):
+        ans = [[0]*N for _ in range(N)]
+        for i in range(N):
+            for j in range(N):
+                ans[i][j] = rm[N-j-1][i]
+        return ans
+    def go(N,rm):
+        ans = [[0]*N for _ in range(N)]
+        for x in range(N):
+            st = []
+            for y in range(N):
+                if rm[x][y] !=0:
+                    st.append(rm[x][y])
 
-elif S == 'Left':
-    rm = f(N,rm)
-    rm = f(N,rm)
-    go(N,rm)
-elif S == 'Down':
-    rm = f(N,rm)
-    rm = f(N,rm)
-    rm = f(N,rm)
-    go(N,rm)
+            j = N-1
+            # print(st)
+            while len(st) >=2:
+                a = st.pop()
+                b = st.pop()
+                if a == b:
+                    ans[x][j] = a+b
+                elif a != b:
+                    ans[x][j] = a
+                    st.append(b)
+                j-=1
+            if len(st) == 1:
+                # print(st)
+                ans[x][j] = st.pop()
+        return ans
 
+    n = input().split()
+    N= int(n[0])
+    S= n[1]
+    rm = [list(map(int,input().split())) for _ in range(N)]
+    ans = []
+    if S =='right':
+        ans = go(N,rm)
+    elif S=='up':
+        rm = f(N,rm)
+        ans = go(N,rm)
+        for _ in range(3):
+            ans = f(N,ans)
+
+    elif S == 'left':
+        rm = f(N,rm)
+        rm = f(N,rm)
+        ans = go(N,rm)
+        for _ in range(2):
+            ans = f(N,ans)
+    elif S == 'down':
+        rm = f(N,rm)
+        rm = f(N,rm)
+        rm = f(N,rm)
+        ans = go(N,rm)
+        ans = f(N,ans)
+    print("#{}".format(tc+1))
+    for x in range(N):
+        for y in range(N):
+            print(ans[x][y], end = " ")
+        print()
