ae932e6a2a774f041235d9e8736d018dd17d3a37
이은재 <silver_jae@naver.com>
2020-04-01 22:24:26 +0900

    20200401

---
+++ programmers/정렬/가장큰수.py
@@ -0,0 +1,20 @@
+def solution(nums):
+    answer = ''
+    num = []
+    for i in nums:
+        num.append(str(i))
+    print(num)
+    def find(num,n,k):
+        if n == k:
+            return
+        else:
+
+
+
+    find(num,0,len(num))
+
+    return answer
+
+
+nums = [3, 30, 34, 5, 9]
+solution(nums)
