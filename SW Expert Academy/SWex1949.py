8f807d9dd35eb03a609b97914230846f665329e0
이은재 <silver_jae@naver.com>
2020-04-11 17:53:55 +0900

    20200411

--- SW Expert Academy/SWex1949.py
+++ SW Expert Academy/SWex1949.py
@@ -2,26 +2,35 @@
 
 
 def dfs(x,y):
-    global rm,maxV,cnt,k
+    global maxV
     dx = [-1, 1, 0, 0]  # 상하좌우
     dy = [0, 0, -1, 1]
     s = []
+    cnt = 0
     s.append((x,y))
     while s:
         print(s)
-        if rm[x][y] ==1 or rm[x][y] == 1-k:
-            maxV = cnt
-            cnt = 0
-            s.pop()
         sx,sy = s.pop()
-        for k in range(4):
-            nx = sx + dx[k]
-            ny = sy + dy[k]
+        re = True
+        for i in range(4):
+            nx = sx + dx[i]
+            ny = sy + dy[i]
             if 0<=nx<n and 0<=ny<n:
-                # for j in range(k):
-                if rm[sx][sy] > rm[nx][ny]-k:
+                if rm[sx][sy] > rm[nx][ny]:
                     s.append((nx,ny))
                     cnt +=1
+                    break
+                # 이동할 위치가 전 위치보다 크거나 같을경우엔 k범위 내에서 빼준다.
+                elif re == True:
+                    for kk in range(1,k+1):
+                        # 1~ k범위에서 새로운 위치에 빼서 가장 큰 경우의수로
+                        if rm[sx][sy] > rm[nx][ny]-kk:
+                            s.append((nx,ny))
+                            cnt +=1
+                            re = False # k는 한번만 사용한다
+                            break
+                    break
+    return cnt
 
 
 n,k = map(int,input().split())
@@ -29,12 +38,12 @@ rm = [list(map(int,input().split())) for _ in range(n)]
 maxV = 0
 cnt = 1
 # 가장 큰 높이를 알기위한 알고리즘
-for i in range(n):
-    for j in range(n):
-        if rm[i][j] > maxV:
-            maxV = rm[i][j]
+for i in rm:
+    if max(i) > maxV:
+        maxV = max(i)
+print(maxV)
 for x in range(n):
     for y in range(n):
         if rm[x][y] == maxV:
-            dfs(x,y)
-            print(cnt)
+            print(dfs(x,y))
+
