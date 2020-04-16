dc9e6d0180d26a0e6baa24d3716b7c516ab51715
이은재 <silver_jae@naver.com>
2020-04-16 20:04:08 +0900

    20200416

--- SW Expert Academy/SWex2115.py
+++ SW Expert Academy/SWex2115.py
@@ -2,30 +2,32 @@
 # a = [16,6,5,3,4,1,6,7,13,36,675]
 # a = sorted(a,reverse=True)
 # print(a)
-def per(arr,l,m):
+def dfs(x,y,n,k):
+    global used
+    if n == k:
+        for i in used:
+            if i == 1:
 
-    if l == m:
-
-        for i in range(k):
-            if used[i] == 0:    # i번 원소가 사용되지 않았다면
-                used[i] = 1     # 사용함으로 표시
-                p[n] = A[i]
-                f(n+1,k)        # n+1 원소결정
-                used[i] = 0
+        print(used)
     else:
-        for i in range(l):
-            if used[i] == 0:
-                used[i] = 1
-        per(arr,l+1,m)
-
-        per(arr, l + 1, m)
+        used[n] = 1
+        (x,y,n+1,k)
+        used[n] = 0
+        (x, y, n + 1, k)
 
 
 n,m,c = map(int,input().split())
 rm = [list(map(int,input().split())) for _ in range(n)]
 total = []     # 가장 큰 두자리를 위한것
+used = [0]*m
 for i in range(n):
     for j in range(n-m+1):
+        if sum(rm[i][j:j + m]) == c or sum(rm[i][j:j+m]) >= c:
+            dfs(i,j,0,m)
+            for x in range(i,n):
+                for y in range(j,n-m+1):
+                    if sum(rm[x][y:y + m]) == c or sum(rm[x][y:y + m]) >= c:
+
         re = comp=0
         # c와 같을경우
         if sum(rm[i][j:j+m]) == c:
---
+++ python개념/Tree/이진탐색트리.py
@@ -0,0 +1,11 @@
+# 연결리스트로 구현해보자
+
+'''
+연결리스트
+             /left/data/right/
+            (               )
+/left/data/right/       /left/data/right/
+'''
+
+class Node:
+    def __init__(self,d,l=):
