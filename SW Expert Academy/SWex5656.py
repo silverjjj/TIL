67101527fe29a6dd2d12448d6b4c9ed069c056f4
이은재 <silver_jae@naver.com>
2020-04-17 19:44:40 +0900

    20200417

---
+++ SW Expert Academy/SWex5656.py
@@ -0,0 +1,12 @@
+# SWex5656. [모의 SW 역량테스트] 벽돌 깨기
+
+
+# rm 만드기
+N,W,H = map(int,input().split())
+rm = [list(int,input().split()) for _ in range(H)]
+
+# 순열로 때릴 경우의수
+
+# 1번 때릴때 해당되는 경우를 다 0으로 하고
+
+# 벽돌정리
---
+++ 클래스 사용법.py
@@ -0,0 +1,7 @@
+class robot:
+    name = '이은재'
+    def info(self):
+        print('내이름은', self.name, '입니다')
+
+r = robot()
+print(r.name)
