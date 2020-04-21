84ed3b54dce85228ead5cc816edc6d5e0ee14575
이은재 <silver_jae@naver.com>
2020-04-21 23:39:33 +0900

    TIL

--- SW Expert Academy/SWex5656.py
+++ SW Expert Academy/SWex5656.py
@@ -1,6 +1,12 @@
 # SWex5656. [모의 SW 역량테스트] 벽돌 깨기
 import itertools
 
+# 현재 수정해야할곳
+# 벽돌 제거후 떨어지는부분 보완해야함 << 완전고쳐야함
+# 그리고 순열은 누적도 0,0,0 이렇게 가능해야하는데 이런게 다빠져있는상태
+
+
+
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
@@ -10,17 +16,28 @@ def after(rm):
         print(row)
     print('-------------------------------')
 
+    # 벽돌 떨어지는부분
     for j in range(W):
         for i in range(H-1,1,-1):
-            if rm[i][j] == 0:
-                rm[i][j],rm[i-1][j] = rm[i-1][j],rm[i][j]
+            if rm[i][j] != 0:
+                while rm[i+1][j] != 0 or i+1 != H-1:
+                    rm[i][j], rm[i + 1][j] = rm[i + 1][j], rm[i][j]
 
+
+        #     if rm[i][j] != 0:
+        #         tmp.append(rm[i][j])
+        # rm[]
+        #         rm[i][j],rm[i-1][j] = rm[i-1][j],rm[i][j]
+    for row in rm:
+        print(row)
+    print('-------------------------------')
 
 
 def drop(p):
     print(p)
     s = []
     for j in p:
+        visit = [[0] * W for _ in range(H)]
         for i in range(H):
             # 0이 아닌곳에 부딪힌다.
             if rm[i][j] !=0:
@@ -28,17 +45,26 @@ def drop(p):
                 # rm[i][j] = 0
                 while s:
                     x,y = s.pop()
-                    for l in range(0, rm[i][j]):
+                    print(x,y,rm[x][y])
+                    for l in range(0, rm[x][y]):
                         for k in range(4):
                             nx = x + dx[k]*l
                             ny = y + dy[k]*l
-                            if 0<=nx<H and 0<=ny<W and rm[nx][ny] !=0:
+                            if 0<=nx<H and 0<=ny<W and rm[nx][ny] != 0 and visit[nx][ny] ==0:
                                 s.append([nx,ny])
-                                print(s,rm[nx][ny])
-                                rm[nx][ny] = 0
+                                # rm[nx][ny] = 0
+                                visit[nx][ny] = 1
 
+                # 폭탄 터지는부분
+                for i in range(H):
+                    for j in range(W):
+                        if visit[i][j] == 1:
+                            rm[i][j] = 0
+                for row in visit:
+                    print(row)
+                print('방문자표시~')
+                after(rm)
                 break
-    after(rm)
 
 
 def per():
@@ -53,6 +79,7 @@ def per():
 # rm 만드기
 N,W,H = map(int,input().split())
 rm = [list(map(int,input().split())) for _ in range(H)]
+
 
 # 순열로 때릴 경우의수
 # W개중 3개를 고르자
