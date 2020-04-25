44149787e86aeffb46b2b8261a72524a682f91ca
이은재 <silver_jae@naver.com>
2020-04-25 21:51:01 +0900

    20200425

--- SW Expert Academy/SWex5656.py
+++ SW Expert Academy/SWex5656.py
@@ -1,45 +1,24 @@
-# SWex5656. [모의 SW 역량테스트] 벽돌 깨기
+# # SWex5656. [모의 SW 역량테스트] 벽돌 깨기
 import itertools
 
-# 현재 수정해야할곳
-# 벽돌 제거후 떨어지는부분 보완해야함 << 완전고쳐야함
-# 그리고 순열은 누적도 0,0,0 이렇게 가능해야하는데 이런게 다빠져있는상태
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-
 def after(rm):
-    # for row in rm:
-    #     print(row)
-    # print('-------------------------------')
-
-    # 벽돌 떨어지는부분
     for j in range(W):
         tmp = []
         for i in range(H-1,0,-1):
             if rm[i][j] != 0:
                 tmp.append(rm[i][j])
                 rm[i][j] = 0
-        # print(tmp)
-
         for k in range(H-1,H-1-len(tmp),-1):
             rm[k][j] = tmp.pop(0)
-    #
-    #
-    # print("벽돌 떨어짐")
-    # for row in rm:
-    #     print(row)
-    # print("떨어짐 완료")
-
 
 def drop(p,rm):
-    # print(p)
+    # 중복순열 p가 들어온다.
     s = []
     for j in p:
         visit = [[0] * W for _ in range(H)]
-        # for row in visit:
-        #     print(row)
-        # print('-------------------------')
         for i in range(H):
             # 0이 아닌곳에 부딪힌다.
             if rm[i][j] !=0:
@@ -62,47 +41,30 @@ def drop(p,rm):
                     for j in range(W):
                         if visit[i][j] == 1:
                             rm[i][j] = 0
-                # for row in visit:
-                #     print(row)
-                # print('방문자표시~')
                 after(rm)
                 break
 
-
-
-
-N,W,H = map(int,input().split())
-room = [list(map(int,input().split())) for _ in range(H)]
-# 순열로 때릴 경우의수
-# W개중 3개를 고르자
-use = [0]*W
-for i in range(W):
-    use[i] = i
-# print(use)
-minV = 123456789
-permu = list(itertools.permutations(use,3))
-# print(permu)
-
-for p in permu:
-    # print(p)
-    rm = []
-
-    # 하나의 p가 갈때마다 새로운 rm으로 가줘야하는데
-    # 그렇게 하기 위해 계속해서 room을 이용해 rm을 만들어줌
-    # 근데 한바퀴 돌때마다 room이 영향을 받아 값이 변함.
-    for row in room:
-        rm.append(row)
-
-    print('---------------')
-    drop(p,rm)
-    for row in room:
-        print(row)
-    cnt = 0
-
-
-    if minV > cnt:
-        minV = cnt
-
+T = int(input())
+for tc in range(1,T+1):
+    N,W,H = map(int,input().split())
+    room = [list(map(int,input().split())) for _ in range(H)]
+    minV = 123456789
+    # 중복 순열 만들기 W개중에서 N개를 가진 중복순열을 만든다.
+    permu = list(itertools.product(range(W),repeat=N))
+    for p in permu:
+        rm = [[0] * W for _ in range(H)]
+        for i in range(H):
+            for j in range(W):
+                rm[i][j] = room[i][j]
+        drop(p,rm)
+        cnt = 0
+        for ro in rm:
+            for r in ro:
+                if r != 0:
+                    cnt +=1
+        if minV > cnt:
+            minV = cnt
+    print("#{} {}".format(tc,minV))
 '''
 3 10 10
 0 0 0 0 0 0 0 0 0 0
--- 풍선팡.py
+++ 풍선팡.py
@@ -1,13 +1,11 @@
-bases = str(input())
-case = []
-for i in bases:
-    case.append(i)
-print(case)
-card = []
-for i in range(0,len(bases),3):
-    card.append(bases[i:i+3])
-print(card)
-a = list(set(card))
-print(a)
-
-if len(a) == len(card):
+def per(array, N):
+    if len(array) == N:
+        temp = [0] * N
+        for i in range(N):
+            temp[i] = array[i]
+        drop(temp)
+        return
+    for i in range(W):
+        array.append(i)
+        per(array, N)
+        array.pop()
