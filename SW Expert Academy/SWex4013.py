fbbc87c1b6f7b2227240f019b5b40dba04446b36
이은재 <silver_jae@naver.com>
2020-05-12 23:48:21 +0900

    20200512

--- SW Expert Academy/SWex4013.py
+++ SW Expert Academy/SWex4013.py
@@ -1,17 +1,55 @@
-k = int(input())
-arr = [list(map(int,input().split())) for _ in range(4)]
-for row in arr:
-    print(row)
-a = arr[0]
-b = arr[1]
-c = arr[2]
-d = arr[3]
-cnt = 0
-while k > cnt:
-    n, m = map(int, input().split())
-    # i = 1,6이 만나면 돌려짐
-    for i in range(0,3):
-        if arr[i][2] == arr[i+1][6]:
-            
+T= int(input())
+for tc in range(1,T+1):
+    k = int(input())
+    arr = [list(map(int,input().split())) for _ in range(4)]
+    # for row in arr:
+    #     print(row)
+    for _ in range(k):
+        n, m = map(int, input().split())
+        # print(n,m)
+        info = []
+        total = []
+        visited = [0]*4
+        info.append([n-1,m])
+        total.append([n-1,m])
+        while info:
+            # print(info)
+            x,y = info.pop()
+            # print(x,y)
+            visited[x] = 1
+            if 0<=x+1<4 and visited[x+1] == 0:    # 오른쪽
+                if arr[x][2] != arr[x+1][6]:
+                    info.append([x+1,-y])
+                    total.append([x + 1, -y])
+                    visited[x+1] = 1
+            if 0<=x-1<4 and visited[x-1] == 0:   # 왼쪽
+                if arr[x][6] != arr[x-1][2]:
+                    info.append([x - 1, -y])
+                    total.append([x-1,-y])
+                    visited[x-1] = 1
+        # print(visited, total)
+        for i in range(len(total)):
+            if total[i][1] == 1:
+                num = total[i][0]
+                tmp = arr[num].pop(7)
+                arr[num].insert(0,tmp)
+            else:
+                num = total[i][0]
+                tmp = arr[num].pop(0)
+                arr[num].append(tmp)
+    sum = 0
+    for i in range(4):
+        if arr[i][0] == 1:
+            sum += arr[i][0]*(2**i)
+    print("#{} {}".format(tc,sum))
 
-
+'''
+10
+2
+0 0 1 0 0 1 0 0
+1 0 0 1 1 1 0 1
+0 0 1 0 1 1 0 0
+0 0 1 0 1 1 0 1
+1 1
+3 -1
+'''
