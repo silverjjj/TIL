f3aa9fed50c54602808ffe515670103556ce76dc
이은재 <silver_jae@naver.com>
2020-05-23 17:05:37 +0900

    mst_kruskal_prim

--- algorithm/09_그래프/MST/mst_prim_pq.py
+++ algorithm/09_그래프/MST/mst_prim_pq.py
@@ -1,6 +1,7 @@
 '''
 우선순위 queue : heapq
 heapq은 딕셔너리의 key값을 기준으로 배열된 힙을 만들어준다
+heapq.heappush(heap을 위한 list, (우선순위값, 데이터))
 '''
 # 딕셔너리 + 큐로 푼 문제
 import heapq
@@ -13,33 +14,42 @@ for i in range(E):
 print(adj)
 # key, p, mst 준비
 INF = float('inf')  # 무한
-key = [INF]*V
-mst = [False] * V
+key = [INF]*V       # 최단거리를 기록
+mst = [False] * V   # mst를 했냐? 여부의 list
 pq = []
 # 시작 정점 선택 : 0
 key[0] = 0
+num = [0]*(V+1)
 # 큐에 시작 정점을 넣음 => (key, 정점인덱스)
 
 # 우선순위 큐 => 이진합 => heapq 라이브러리 사용=>
-heapq.heappush(pq,(0,0))    # 우선순위큐 -> 원소의 첫번째 요소 -> key를 우선순위로
+# 우선순위 : 0, 데이터 0을 push
+heapq.heappush(pq,(0,0))
+
 result = 0
 while pq:
     print(pq)
     # 최소값 찾기
     k, node = heapq.heappop(pq)
+    print(k,node)
     # print(k,node)
     if mst[node]:   # mst가 true일경우 continue
-        continue
+        continue    # mst의 true 의미 : 이미 이자리엔 최소가중치를 했다.
     # mst로 선택
     mst[node] = True
     result +=k
     # key 갱신 => key배열/큐
     # 해당 노드에서 가중치가 가장 작은걸 데려오기 위한 for문
-    for dest, wt in adj[node]:
-        if not mst[dest] and key[dest] > wt:
-            key[dest] = wt
+    for end, w in adj[node]:   # end : 도착점, wt: 가중치
+        if not mst[end] and key[end] > w:
+            # end자리에 가중치 w를 다시 갱신
+            key[end] = w
+            num[end] = node
             # 큐 갱신 => 새로운 (key,정점) 삽입 => 필요없는 원소는 스킵
-            heapq.heappush(pq,(key[dest], dest))
+            heapq.heappush(pq,(key[end], end))
+print(key)
+print(num)
+print(mst)
 print(result)
 
 
