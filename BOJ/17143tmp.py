dd06ce9b6a658955384f8c70374d696fcea5b183
이은재 <silver_jae@naver.com>
2020-08-23 21:09:18 +0900

    TIL - Trie의 개념 및 예제풀이

--- BOJ/17143tmp.py
+++ BOJ/17143tmp.py
@@ -4,4 +4,4 @@ shark = []
 # r,c,s,d,z
 visited = [[0]*Y for _ in range(X)]
 for m in range(M):
-    x, y, s, d, z = list(map(int,input().split()))
+    x, y, s, d, z = list(map(int,input().split()))\
