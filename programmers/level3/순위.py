7a6cf68b3a287a0ad3b2d80fe893b37d2b120926
이은재 <silver_jae@naver.com>
2020-09-03 18:54:30 +0900

    TIL - algo

--- programmers/level3/순위.py
+++ programmers/level3/순위.py
@@ -10,24 +10,39 @@ results의 각 배열은 A,B를 의미하고 A가 이긴것
 5 :
 를 갖고 정확한 순위를 정할수있는 멤버수를 찾자
 '''
-
+def updown(win,lose,n,num):
+    # print(arr)
+    arr = [0] * (n + 1)
+    q = [num]
+    while q:
+        cur = q.pop(0)
+        if not arr[cur]:
+            arr[cur] = 1
+            for next in win[cur]:
+                q.append(next)
+    q = [num]
+    arr[num] = 0
+    while q:
+        cur = q.pop(0)
+        if not arr[cur]:
+            arr[cur] = 1
+            for next in lose[cur]:
+                q.append(next)
+    return arr
 def solution(n, results):
-    win = {i: set() for i in range(1,n+1)}
-    lose = {i: set() for i in range(1,n+1)}
+    answer=0
+    win = {i: [] for i in range(1,n+1)}
+    lose = {i: [] for i in range(1,n+1)}
     for winner, loser in results:
-        win[winner].add(loser)
-        lose[loser].add(winner)
-        print(lose[loser] ,loser)
-        # for w in win[loser]:
-        #     win[winner].add(w)
-        # for l in win[winner]:
-        #     lose[loser].add(l)
-        print(win)
-        print(lose)
+        win[winner].append(loser)
+        lose[loser].append(winner)
 
-    answer = 0
+    for num in range(1, n + 1):
+        arr = updown(win,lose,n,num)
+        if sum(arr) == n:
+            answer += 1
+    # print(answer)
     return answer
-
 n = 5
 results = 	[[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
 solution(n, results)
