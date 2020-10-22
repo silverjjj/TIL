beb85071f496e6da35b89bb0d2a1794506c03333
이은재 <silver_jae@naver.com>
2020-10-22 22:18:26 +0900

    TIL

--- programmers/숫자게임.py
+++ programmers/숫자게임.py
@@ -1,33 +1,49 @@
+# 1. 분할
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge_sort(left, right)
+
+# 2. 합병
+def merge(left, right):
+    l = r = 0
+    sorted_list = []
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            sorted_list.append(left[l])
+            l += 1
+        else:
+            sorted_list.append(right[r])
+            r+=1
+    while l < len(left):
+        sorted_list.append(left[l])
+        l += 1
+    while r < len(right):
+        sorted_list.append(right[r])
+        r += 1
+    return sorted_list
+
 def solution(A, B):
     answer = 0
-    dictB = {}
-    listB = []
-    for num in B:
-        if num in dictB.keys():
-            dictB[num] += 1
+    A = merge_sort(A)
+    B = merge_sort(B)
+    if A[0] >= B[-1]:
+        return 0
+    while A:
+        maxA = A[-1]
+        maxB = B[-1]
+        if maxA < maxB:     # 각 배열의 최댓값끼리 대결
+            answer += 1
+            B.pop(-1)
+            A.pop(-1)
         else:
-            dictB[num] = 1
-            listB.append(num)
-    listB.sort()
-    res = 0
-    for a in A:
-        if listB[-1] > a:
-            idx = listB.index(a)
-            nexta = listB[idx+1]
-            if nexta in dictB.keys():
-                dictB[nexta] -= 1
-                res += 1
-                if dictB[nexta] == 0:
-                    listB.remove(nexta)
-                    del dictB[nexta]
+            A.pop(-1)
+    # print(answer)
+    return answer
 
-        else:
-            break
-    print(res)
-    return res
-
-A = [5,1,3,7]
-B = [2,2,6,8]
-# A = [2,2,2,2]
-# B = [1,1,1,1]
+A = [2,2,2,2]
+B =[1,1,1,1]
 solution(A, B)
