6a43cbd484132494ad82bd8abc5bc8089cc8f988
이은재 <silver_jae@naver.com>
2020-11-13 20:37:44 +0900

    20201113-알고리즘

--- algorithm/BOJ/16234.py
+++ algorithm/BOJ/16234.py
@@ -1,63 +1,63 @@
 '''
+4 시간 풀었는데 시간초과
+
 1. 두나라의 인구차이가 L<= <=R 이면 국경선을 하루동안 연다
 2. 열리면 "연합" 각칸의 인구수는  연합의 인구수 / 연합국 수, 소수점은 버림
 3. 연합 해체, 국경선을 닫는다.
 '''
 import sys
 from collections import deque
-
-read = sys.stdin.readline
+input = sys.stdin.readline
 dx = [-1,1,0,0]
 dy = [0,0,1,-1]
 
-def count_move():
-    global res, mapping
+def MOVE():
     visited = [[0] * N for _ in range(N)]
-    groups = []
+    groups = {}
+    idx = 0
     for x in range(N):
         for y in range(N):
             if not visited[x][y]:
-                visited[x][y] = 1
-                q = deque([])
-                q.append([x, y])
-                group = [[x, y]]
-                while q:
-                    # print(q)
-                    x, y = q.popleft()
+                idx += 1
+                visited[x][y] = idx
+                dq = deque([])
+                dq.append([x, y])
+                tmp = [[x,y]]
+                sum, cnt = mapping[x][y], 1
+                while dq:
+                    sx, sy = dq.popleft()
                     for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                            if R <= abs(mapping[x][y] - mapping[nx][ny]) <= C:
-                                visited[nx][ny] = 1
-                                q.append([nx, ny])
-                                group.append([nx, ny])
-                # print(group)
-                if len(group) > 1:
-                    groups.append(group)
-    print(groups)
-    if len(groups) >= 1:
-        res += 1
-        for tmp in groups:
-            total = 0
-            for i, j in tmp:
-                total += mapping[i][j]
-            for i, j in tmp:
-                mapping[i][j] = total // len(tmp)
-        return True
-    else:
+                        nx = sx + dx[k]
+                        ny = sy + dy[k]
+                        if 0 <= nx < N and 0 <= ny < N:
+                            if not visited[nx][ny] and L <= abs(mapping[sx][sy] - mapping[nx][ny]) <= R:
+                                visited[nx][ny] = idx
+                                dq.append([nx, ny])
+                                sum += mapping[nx][ny]
+                                cnt += 1
+                                tmp.append([nx,ny])
+                if cnt != 1:
+                    tmp.insert(0,[idx,sum//cnt])
+
+    if not len(groups):
         return False
-N,R,C = map(int,input().split())
-mapping = [list(map(int,read().split())) for _ in range(N)]
+    return tmp
+
+N,L,R = map(int,input().split())
+mapping = [list(map(int,input().split())) for _ in range(N)]
 res = 0
-while True:
-    if not count_move():
-        for r in mapping:
-            print(r)
+flag = True
+while flag:
+    flag = MOVE()
+    if not flag:
         break
-    for r in mapping:
-        print(r)
+
+    for i in range(1,len(flag)):
+        for i, j in val[1]:
+            mapping[i][j] = val[0]
+    res += 1
 print(res)
+
 
 
 
@@ -65,4 +65,43 @@ print(res)
 2 10 20
 0 30
 50 10
+
+2 10 20
+0 20
+50 20
+
+
+4 1 9
+96 93 74 30
+60 90 65 96
+5 27 17 98
+10 41 46 20
+correct: 1
+
+(96,93,90)->93
+(74,65)->69
+(96,98)->97
+(5,10)->7
+(41,46)->43
+
+/************/
+5 1 5
+88 27 34 84 9
+40 91 11 30 81
+2 88 65 26 75
+75 66 16 14 28
+89 10 5 30 75
+correct: 1
+
+(91,88)->89
+(30,26)->28
+(16,14)->15
+(10,5)->7
+
+4 10 50
+10 100 80 90
+30 100 70 70
+40 50 60 140
+50 20 100 10
+
 '''
--- algorithm/BOJ/2186.py
+++ algorithm/BOJ/2186.py
@@ -1,58 +1,52 @@
 '''
 8퍼 시간초과
-
+딕셔너리를 사용하면 문자판에 있는 모든 K의 문자를 담으니까 메모리초과가 발생
+메모이제이션 + dfs
 '''
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-def BFS(st):
-    dq = deque([st])
-    cnt = 1
-    visited = [[0 for _ in range(M)] for _ in range(N)]
-    visited[st[0]][st[1]] += 1
-    while dq and len(chars) > cnt:
-        L = len(dq)
-        # print(dq)
-        for _ in range(L):
-            x,y = dq.popleft()
-            for nx,ny in dict.get(chars[cnt]):
-                if not visited[nx][ny]:
-                    if (abs(x - nx) + abs(y - ny)) <= K:
-                        visited[nx][ny] = 1
-                        dq.append([nx,ny])
-                else:
-                    if (abs(x - nx) + abs(y - ny)) <= K:
-                        visited[nx][ny] = 1 + visited[x][y]
-        cnt += 1
-    # for r in visited:
-    #     print(r)
-    # print(dq)
-    tmp = 0
-    for i,j in dq:
-        tmp += visited[i][j]
-    return tmp
+d = [(0,1),(0,-1),(1,0),(-1,0)]
+def BFS(x, y, idx):
+    if idx == len(char):
+        return 1
+    if visited[x][y][idx] != -1:
+        return visited[x][y][idx]
+
+    visited[x][y][idx] = 0
+    for add in range(1,K+1):
+        for dx,dy in d:
+            nx = x + (dx*add)
+            ny = y + (dy*add)
+            if 0<=nx<N and 0<=ny<M:
+                if room[nx][ny] == char[idx]:
+                    visited[x][y][idx] += BFS(nx, ny, idx+1)
+
+    return visited[x][y][idx]
+
+
 N,M,K = map(int,input().split())
 room = [list(map(str,input())) for _ in range(N)]
-chars = str(input().rstrip())
-dict = {}
-for char in chars:
-    dict[char] = []
-for i in range(M):
-    for j in range(N):
-        if dict.get(room[i][j]) != None:
-            dict[room[i][j]].append([i,j])
-res = 0
-for st in dict.get(chars[0]):
-    res += BFS(st)
-print(res)
+char = str(input().rstrip())
+chars = []
+for i in range(N):
+    for j in range(M):
+        if room[i][j] == char[0]:
+            chars.append([i,j])
+
+visited = [[[-1]*len(char) for _ in range(M)] for _ in range(N)]  # 3차원 리스트로 위치 and 방문표시
+ans = 0
+for x,y in chars:
+    ans += BFS(x,y,1)
+print(ans)
 
 '''
 
 
-4 4 3
+4 4 2
 KAKT
-XEAS
+XEAE
 YREU
 ZBBR
 BREAK
