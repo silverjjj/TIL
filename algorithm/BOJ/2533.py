4d242d7c1be25ecb74b8c8b12c80ce33e7cca2c3
이은재 <silver_jae@naver.com>
2020-11-16 23:17:17 +0900

    20201116-ADSP 및 알고리즘

--- algorithm/BOJ/2533.py
+++ algorithm/BOJ/2533.py
@@ -1,25 +1,27 @@
+'''
+4%에서 틀림
+'''
+
 import sys
 from collections import deque
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-def BFS(node):
-    dq = deque([node])
-    two = [1, 0]  # 홀 ,짝
-    depth = 0
-    while dq:
-        L = len(dq)
-        depth += 1
-        cnt = 0
-        for _ in range(L):
-            cur = dq.popleft()
-            if visited[cur]:
-                continue
-            visited[cur] = 1
-            for next in dict[cur]:
-                if not visited[next]:
-                    cnt += 1
-                    dq.append(next)
-        two[depth % 2] += cnt
-    return two
+def BFS(dq,depth):
+    L = len(dq)
+    if L == 0:
+        return
+    res[depth%2] += L
+    for _ in range(L):
+        cur = dq.popleft()
+        if visited[cur]:
+            continue
+        visited[cur] = 1
+        for next in dict[cur]:
+            if not visited[next]:
+                dq.append(next)
+    BFS(dq,depth+1)
+    return res
+
 N = int(input())
 dict = { i : [] for i in range(1,N+1)}
 for _ in range(N-1):
@@ -28,12 +30,8 @@ for _ in range(N-1):
     dict[v].append(u)
 visited = [0 for _ in range(N+1)]
 res = [0,0]
-for i in range(1, N+1):
-    if not visited[i]:
-        two = BFS(i)
-        res[0] += two[0]
-        res[1] += two[1]
-print(min(res))
+dq = deque([1])
+print(min(BFS(dq,0)))
 
 '''
 5
