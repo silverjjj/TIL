61ee571e418fa274d6adbd88a16c5be86dc609b8
이은재 <silver_jae@naver.com>
2020-11-18 12:12:07 +0900

    20201118-알고리즘

--- algorithm/BOJ/10799.py
+++ algorithm/BOJ/10799.py
@@ -1,14 +1,22 @@
+'''
+10799
+python은 1초에 1천만번 계산이 가능
+해당 문제는 N이 최대 10만이기때문에 10만^2 = 천억이 나옴
+그래서 해당 문제의 시간복잡도는 O(N)으로 풀어야함
+'''
+
 import sys
 input = sys.stdin.readline
 chars = str(input().rstrip())
-s = []
-lines = []
-lasers = [0]*len(chars)
+stack = []
 res = 0
 for i in range(len(chars)):
-    if chars[i] == '(':
-        s.append('(')
-    elif chars[i] == ')':
-        char = s.pop()
-        res += len(s)
-    print(res)
+    if chars[i] == "(":
+        stack.append(["(",i])
+    else:
+        char,idx = stack.pop()
+        if i-idx == 1:
+            res += len(stack)
+        else:
+            res += 1
+print(res)
