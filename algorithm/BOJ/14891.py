86cf89fa39c694371102ae0f97e36d466c0a1e89
이은재 <silver_jae@naver.com>
2020-12-04 22:58:12 +0900

    20201204-알고리즘

--- algorithm/BOJ/14891.py
+++ algorithm/BOJ/14891.py
@@ -1,47 +1,35 @@
+import sys
+input = sys.stdin.readline
 
-
-def simulation(arr,num,d):
-    print("============================")
-    s = [[num,d]]
-    visited = [0] * 4
-    while s:
-        print(s)
-        num,d = s.pop()
-        # 방문표시
-        if visited[num]:
-            continue
-        visited[num] = 1
-        if num == 0:
-            if arr[num][2] != arr[num + 1][-2]:
-                s.append([num+1,-d])
-        elif num == 3:
-            if arr[num][2] != arr[num - 1][-2]:
-                s.append([num-1,-d])
-        else:
-            if arr[num][2] != arr[num - 1][-2]:
-                s.append([num-1,-d])
-            if arr[num][2] != arr[num + 1][-2]:
-                s.append([num+1,-d])
-
+def rotate(tmp):
+    for num, d in tmp:
         if d == -1:
-            char = arr[num]
-            first = char.pop(0)
-            arr[num].append(first)
+            tmp = arr[num].pop(0)
+            arr[num].append(tmp)
         elif d == 1:
-            char = arr[num]
-            last = char.pop()
-            arr[num].insert(0,last)
-    return arr
-arr = []
-for _ in range(4):
-    char= list(str(input()))
-    arr.append(char)
+            tmp = arr[num].pop(-1)
+            arr[num].insert(0,tmp)
+
+def simulation(num,d):
+    if visited[num]:
+        return
+    visited[num] = 1
+    tmp.append([num,d])
+    if 0<=num+1<=3 and arr[num][2] != arr[num+1][6]:
+        simulation(num+1, -d)
+    if 0<= num-1 <= 3 and arr[num][6] != arr[num-1][2]:
+        simulation(num-1, -d)
+
+arr = [list(map(int,input())) for _ in range(4)]
 n = int(input())
 for _ in range(n):
-    num, d = map(int,input().split())
-    arr = simulation(arr,num-1,d)
+    number, d = map(int,input().split())
+    visited = [0]*4
+    tmp = []
+    simulation(number-1,d)
+    rotate(tmp)
 res = 0
 for i in range(4):
-    if arr[i][0] == '1':
+    if arr[i][0] == 1:
         res += (2**i)
 print(res)
