bcc28db748c08d52cf71727f45b7826d06b1c3fa
이은재 <silver_jae@naver.com>
2020-12-06 16:29:20 +0900

    20201206-알고리즘

--- algorithm/BOJ/17144.py
+++ algorithm/BOJ/17144.py
@@ -8,27 +8,54 @@ input = sys.stdin.readline
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
+d = ((0,1),(1,0),(0,-1),(-1,0))
+
+# def Diffusion(room):
+#     visited = [[0 for _ in range(M)] for _ in range(N)]
+#     for i in range(N):
+#         for j in range(M):
+#             if (not room[i][j] or mapping[i][j] == -1): # 미세먼지가 없거나 공기청정기
+#                 continue
+#             tmp = []
+#             cnt2 = 0
+#             for k in range(4):
+#                 nx = i + dx[k]
+#                 ny = j + dy[k]
+#                 if 0<=nx<N and 0<=ny<M and mapping[nx][ny] == 0:
+#                     tmp.append([nx,ny,room[i][j]//5])
+#                     cnt2 += 1
+#             tmp.append([i,j,(room[i][j] - (room[i][j]//5)*cnt2)])
+#             for x,y,w in tmp:
+#                 visited[x][y] += w
+#
+#     return visited[:]
 
 
-def Diffusion(room):
-    visited = [[0 for _ in range(M)] for _ in range(N)]
+def Diffusion():
+    dusts = {}
     for i in range(N):
         for j in range(M):
-            if (not room[i][j] or mapping[i][j] == -1): # 미세먼지가 없거나 공기청정기
+            if mapping[i][j] == 0:
+                dusts[str(i) + str(j)] = 0
                 continue
-            tmp = []
-            cnt2 = 0
-            for k in range(4):
-                nx = i + dx[k]
-                ny = j + dy[k]
-                if 0<=nx<N and 0<=ny<M and mapping[nx][ny] == 0:
-                    tmp.append([nx,ny,room[i][j]//5])
-                    cnt2 += 1
-            tmp.append([i,j,(room[i][j] - (room[i][j]//5)*cnt2)])
-            for x,y,w in tmp:
-                visited[x][y] += w
+            elif mapping[i][j] == -1:
+                dusts[str(i) + str(j)] = -1
+                continue
 
-    return visited[:]
+            dusts[str(i)+str(j)] = mapping[i][j]
+            next = mapping[i][j] // 5
+            cnt = 0
+            for dx,dy in d:
+                nx = i + dx
+                ny = j + dy
+                if 0<=nx<N and 0<=ny<M and mapping[nx][ny] > 0:
+                    cnt += 1
+                    if dusts.get(str(nx)+str(ny)) is None:
+                        dusts[str(nx)+str(ny)] = next
+                    else:
+                        dusts[str(nx)+str(ny)] += next
+            dusts[str(i)+str(j)] -= (next * cnt)
+    print(dusts)
 N,M,T = map(int,input().split())
 mapping = []    # 집
 cleaner = []    # 청소기 위치
@@ -38,16 +65,21 @@ for i in range(N):
         cleaner.append(i)
     mapping.append(arr)
 print(cleaner)
-cnt = 0
-room = mapping[:]
-while cnt <= T:
-    cnt += 1
-    Diffusion(room)
-    aircon()
-    for row in room:
-        print(row)
-    # print("=======================")
-    # for row in mapping:
-    #     print(row)
 
-# print(cleaner)
+for _ in range(T):
+    Diffusion()
+for r in mapping:
+    print(r)
+
+
+# while cnt <= T:
+#     cnt += 1
+#     Diffusion(room)
+#     aircon()
+#     for row in room:
+#         print(row)
+#     # print("=======================")
+#     # for row in mapping:
+#     #     print(row)
+#
+# # print(cleaner)
