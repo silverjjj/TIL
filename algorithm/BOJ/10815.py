afb38fdabb539766725ceb7e86c059b2918bdd1c
이은재 <silver_jae@naver.com>
2020-12-13 15:30:04 +0900

    20201213-알고리즘

--- algorithm/BOJ/10815.py
+++ algorithm/BOJ/10815.py
@@ -8,7 +8,9 @@ N = int(input())
 arr = list(map(int,input().split()))
 M = int(input())
 tmp = list(map(int,input().split()))
-visited = ['0']*M
+dist = {}
+for num in tmp:
+    dist[num] = 0
 target = copy.deepcopy(tmp)
 target.sort()
 for i in range(N):
@@ -18,11 +20,12 @@ for i in range(N):
     while left <= right:
         mid = (right + left) // 2
         if target[mid] == num:
-            idx = tmp.index(target[mid])
-            visited[idx] = '1'
+            dist[num] = 1
             break
         if num > target[mid]:
             left = mid + 1
         else:
             right = mid - 1
-print(" ".join(visited))
+
+for val in dist.values():
+    print(val,end =" ")
